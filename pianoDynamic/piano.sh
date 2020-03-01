c0fe1191d2577e39d5134a022ba1ec28ecb87997
Alok <alokdinesh11@gmail.com>
2020-03-01 21:18:36 +0530

    shell update

--- pianoDynamic/piano.sh
+++ pianoDynamic/piano.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
-
 read foo
-echo $foo
 IFS=';' read -ra ADDR <<< $foo
-# echo ${ADDR[0]}
-echo ${ADDR[1]}
 
-# performance_rnn_generate \
-# --config=multiconditioned_performance_with_dynamics \
-# --bundle_file=./models/multiconditioned_performance_with_dynamics.mag \
-# --output_dir=./out/piano \
-# --num_outputs=10 \
-# --num_steps=3000 \
-# --pitch_class_histogram="$1" \
-# --temperature=$2
+performance_rnn_generate \
+--config=multiconditioned_performance_with_dynamics \
+--bundle_file=./models/multiconditioned_performance_with_dynamics.mag \
+--output_dir=./out/piano \
+--num_outputs=10 \
+--num_steps=3000 \
+--pitch_class_histogram="${ADDR[0]}" \
+--temperature=${ADDR[1]}
